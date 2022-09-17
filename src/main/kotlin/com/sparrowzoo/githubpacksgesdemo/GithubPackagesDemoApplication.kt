c95848e9f370e8f656caf7a676cdfa799d89104b
bigduu <mugeng.du@qq.com>
2022-09-17 21:38:22 +0800

    First commit

---
+++ src/main/kotlin/com/sparrowzoo/githubpacksgesdemo/GithubPackagesDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.sparrowzoo.githubpacksgesdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubPackagesDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<GithubPackagesDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/sparrowzoo/githubpacksgesdemo/GithubPackagesDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sparrowzoo.githubpacksgesdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubPackagesDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
